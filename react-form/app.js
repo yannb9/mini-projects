992ac2a9ae088f4aeaa5d2ff0c8604b60e920e11
Yann Bohbot <yannbohbot@gmail.com>
2020-07-16 15:56:13 +0300

    appjs

--- react-form/app.js
+++ react-form/app.js
@@ -4,23 +4,12 @@ var bodyParser = require('body-parser')
 const app = express();
 var path = require('path')
 
-app.get('/',(req,res)=>{res.send('Yann Bohbot')})
 app.use(express.static('src'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.listen(3000, () => console.log('listening on port 3000!'));
 
-app.use(express.urlencoded({
-    extended: true
-  }))
-
-//   app.post('/submit-form', (req, res) => {
-//     console.log(req.body);
-//     res.end()
-//   })
-
-app.post('/submit-react', (req, res) => {
-//     console.', urlencodedParser, function (req, res) {  
-    // Prepare output in JSON format  
-    console.log(req);  
-    res.end();  
+app.post('/api', (req, res) => {
+    console.log(req.body);  
+    // res.end();  
  })  
-
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
